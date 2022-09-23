aa12501721fc630064629ae0e65006711bfa136b
Arthur Mendes <thumendess@gmail.com>
2022-09-23 16:55:05 -0300

    Pushing the lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -12,6 +12,7 @@ export interface LogPayload {
     message: string;
     data?: JSONObjectType;
     session?: JSONObjectType;
+    tags?: string[];
 }
 export declare const transporters: {
     console: () => winston.transports.ConsoleTransportInstance;
@@ -21,25 +22,56 @@ interface LoggerOptions {
     path: string;
     logger?: winston.Logger;
     session?: LogPayload["session"];
+    tags?: LogPayload["tags"];
 }
 export declare class Logger {
     private _logger;
     private _session?;
+    private _tags?;
     private _path;
     private _consoleTransporter;
     private _fileTransporter;
     constructor(options: LoggerOptions);
     private extend;
+    /**
+     * Create a new instance of Logger extending the session data
+     */
+    withSession(data: LogPayload["data"]): Logger;
+    /**
+     * Create a new instance of Logger extending the tags data
+     */
+    withTags(...data: string[]): Logger;
+    /**
+     * Extends this istance of Logger with new session data
+     */
+    addSession(data: LogPayload["data"]): () => void;
+    /**
+     * Extends this istance of Logger with new tags data
+     */
+    addTag(...data: string[]): () => void;
+    /**
+     * Remove the session data in this instance
+     */
+    private removeSession;
+    /**
+     * Remove the tags data in this instance
+     */
+    private removeTag;
+    /**
+     * Clear all session in this instance
+     */
+    clearSession(): void;
+    /**
+     * Clear all tags in this instance
+     */
+    clearTags(): void;
     private log;
-    setData(data: LogPayload["data"]): Logger;
-    use(data: LogPayload["data"]): () => void;
-    private remove;
-    divider(): this;
     info(message: string, data?: Record<string, any>): this;
     warn(message: string, data?: Record<string, any>): this;
     error(message: string, data?: Record<string, any>): this;
     success(message: string, data?: Record<string, any>): this;
     debug(message: string, data?: Record<string, any>): this;
+    divider(): this;
     timer(name: string): {
         finish: () => void;
     };
