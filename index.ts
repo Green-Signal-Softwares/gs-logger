9db062ec6f43110f6abfe4a4a6f73361b04b7bc0
Arthur Mendes <thumendess@gmail.com>
2022-09-23 08:36:04 -0300

    Setup do projeto

---
+++ index.ts
@@ -0,0 +1,199 @@
+import winston from "winston";
+import DailyRotateFile, { DailyRotateFileTransportOptions } from "winston-daily-rotate-file";
+import chalk, { ChalkInstance } from "chalk";
+import superjson from "superjson";
+import path from "path";
+
+export type JSONType = string | number | boolean | undefined | JSONObjectType | Array<JSONType>;
+export type JSONObjectType = { [x: string]: JSONType };
+export type CommonLog<T> = T & { level: string; timestamp: string };
+export interface LogPayload {
+  message: string;
+  data?: JSONObjectType;
+  session?: JSONObjectType;
+}
+
+const myCustomLevels = {
+  levels: {
+    error: 0,
+    warn: 1,
+    info: 2,
+    success: 3,
+    debug: 4,
+  },
+  colors: {
+    error: chalk.red,
+    warn: chalk.yellow,
+    info: chalk.cyan,
+    success: chalk.green,
+    debug: chalk.gray,
+  },
+};
+
+function getStringDate(date: Date) {
+  const hours = `${date.getHours()}`.padStart(2, "0");
+  const minutes = `${date.getMinutes()}`.padStart(2, "0");
+  const seconds = `${date.getSeconds()}`.padStart(2, "0");
+
+  const day = `${date.getDate()}`.padStart(2, "0");
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const year = `${date.getFullYear()}`.padStart(4, "0");
+
+  const timeString = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  return timeString;
+}
+
+function parseJsonToLog({ timestamp, message, level, data }: CommonLog<LogPayload>) {
+  const colors = myCustomLevels.colors as Record<string, ChalkInstance>;
+  const date = chalk.gray(`[${getStringDate(new Date(timestamp))}]`);
+
+  const processed = message
+    .replace(/\*(.*?)\*/g, (match, p1) => {
+      return chalk.bold(p1);
+    })
+    .replace(/\_(.*?)\_/g, (match, p1) => {
+      return chalk.italic(p1);
+    });
+
+  const logs = [`${date} ${colors[level](processed)}`];
+
+  if (data) {
+    logs.push(JSON.stringify(data, null, 2));
+  }
+
+  return logs.join(" ");
+}
+
+const rotateFileTransport = ({
+  filepath = "logs",
+  filename = "%DATE%",
+  datePattern = "YYYY-MM-DD",
+  maxSize = "20m",
+  ...rest
+}: Record<string, any> & DailyRotateFileTransportOptions) => {
+  const finalFileName = path.resolve(__dirname, "..", "..", "..", filepath, `${filename}.log`);
+
+  return new DailyRotateFile({
+    // @ts-ignore
+    filename: finalFileName,
+    datePattern,
+    maxSize,
+    ...rest,
+  });
+};
+
+const consoleFormat = () => {
+  return winston.format.printf((obj) => {
+    return parseJsonToLog(obj as CommonLog<LogPayload>);
+  });
+};
+
+const fileFormat = () => {
+  return winston.format.printf((obj) => {
+    return JSON.stringify(obj);
+  });
+};
+
+export const transporters = {
+  console: new winston.transports.Console({
+    format: winston.format.combine(winston.format.timestamp(), consoleFormat()),
+    handleExceptions: true,
+    level: "debug",
+  }),
+  file: rotateFileTransport({
+    format: winston.format.combine(winston.format.timestamp(), fileFormat()),
+    handleExceptions: true,
+    level: "debug",
+  }),
+};
+
+export class Logger {
+  private constructor(private _logger: winston.Logger, private _session?: LogPayload["data"]) {}
+
+  static create() {
+    return new Logger(winstonLogger);
+  }
+
+  private extend({ logger = this._logger, session = this._session } = {}) {
+    return new Logger(logger, session);
+  }
+
+  private log(
+    getLogMethod: (logger: winston.Logger) => winston.LeveledLogMethod,
+    message: LogPayload["message"],
+    data?: LogPayload["data"]
+  ) {
+    const payload: LogPayload = {
+      message,
+      data: superjson.serialize(data).json as LogPayload["data"],
+      session: superjson.serialize(this._session).json as LogPayload["session"],
+    };
+
+    getLogMethod(this._logger)(payload);
+    return this;
+  }
+
+  public setData(data: LogPayload["data"]) {
+    return this.extend({ session: { ...this._session, ...data } });
+  }
+
+  public use(data: LogPayload["data"]) {
+    this._session = { ...this._session, ...data };
+    return this.remove(data);
+  }
+
+  private remove(data: LogPayload["data"]) {
+    return () => {
+      if (!data || !this._session) return;
+      for (const key of Object.keys(data)) delete this._session[key];
+    };
+  }
+
+  divider() {
+    const divider = Array.from({ length: 80 }, () => "-").join("");
+    return this.debug(divider);
+  }
+
+  info(message: string, data?: Record<string, any>) {
+    return this.log((logger) => logger.info, message, data);
+  }
+
+  warn(message: string, data?: Record<string, any>) {
+    return this.log((logger) => logger.warn, message, data);
+  }
+
+  error(message: string, data?: Record<string, any>) {
+    return this.log((logger) => logger.error, message, data);
+  }
+
+  success(message: string, data?: Record<string, any>) {
+    // @ts-ignore
+    return this.log((logger) => logger.success, message, data);
+  }
+
+  debug(message: string, data?: Record<string, any>) {
+    return this.log((logger) => logger.debug, message, data);
+  }
+
+  timer(name: string) {
+    let seconds = 0;
+
+    const timer = setInterval(() => (seconds += 0.25), 250);
+    return {
+      finish: () => {
+        clearInterval(timer);
+        this.info(`${name}: *${seconds.toFixed(1)}s*`);
+      },
+    };
+  }
+
+  disableFile() {
+    this._logger.remove(transporters.file);
+    return this;
+  }
+
+  enableFile() {
+    this._logger.add(transporters.file);
+    return this;
+  }
+}
